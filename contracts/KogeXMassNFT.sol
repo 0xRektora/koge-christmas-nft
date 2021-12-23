a218a287d1eeef6291d354297d2612bafec873a1
Sadek Walid Mendi <Bloodyline01@gmail.com>
2021-12-23 17:58:58 -0500

    Fixed contract requirement

--- contracts/KogeXMassNFT.sol
+++ contracts/KogeXMassNFT.sol
@@ -17,11 +17,14 @@ contract KogeXMassNFT is ERC721Enumerable, Ownable {
     IERC20 constant kogecoin = IERC20(0x13748d548D95D78a3c83fe3F32604B4796CFfa23);
     IERC20 constant vKogeKoge = IERC20(0x992Ae1912CE6b608E0c0d2BF66259ab1aE62A657);
 
+    uint256 constant endReward = 1640303999;
+
     constructor(string memory __baseUri) ERC721('KogeXMass', 'KGXM') {
         baseUri = __baseUri;
     }
 
     function mint() external returns (uint256) {
+        require(block.timestamp < endReward);
         require(kogecoin.balanceOf(msg.sender) >= (10**9) * 50 || vKogeKoge.balanceOf(msg.sender) >= (10**9) * 20);
         tokenIds.increment();
         uint256 newItemId = tokenIds.current();
